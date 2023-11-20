90bda64c00d20a75006ad53e7fce5387a0891249
Rob Brennan <rob@therobbrennan.com>
2023-11-20 15:38:23 -0800

    Add conversational memory to our chat bot

--- src/chat.py
+++ src/chat.py
@@ -2,8 +2,9 @@ import os
 from dotenv import load_dotenv
 from langchain.vectorstores.redis import Redis
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
 # Load environment variables from .env file
 load_dotenv()
@@ -26,11 +27,14 @@ model = ChatOpenAI(
     temperature= 0,
     model_name= 'gpt-3.5-turbo',
 )
-qa = RetrievalQA.from_llm(llm=model, retriever=retriever)
+
+# Add conversational memory to our chat bot
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+qa = ConversationalRetrievalChain.from_llm(llm=model, retriever=retriever, memory=memory)
 
 print("Ask any question regarding The Four Corners in California book:")
 # keep the bot running in a loop to simulate a conversation
 while True:
     question = input()
-    result = qa({"query": question})
-    print (result["result"])
+    result = qa({"question": question})
+    print (result["answer"])
